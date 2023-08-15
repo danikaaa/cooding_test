0a98a1970dcf9ac921d498f2e881e7cc48580bcb
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2023-08-16 02:20:54 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
-    const asc = numbers.sort((a,b) => b-a);
+    numbers.sort((a,b) => b-a);
     const first = numbers[0]*numbers[1];
     const last = numbers[numbers.length -1]*numbers[numbers.length -2];
     return first >= last ? first : last;
