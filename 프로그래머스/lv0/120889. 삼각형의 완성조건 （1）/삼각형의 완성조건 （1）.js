fbb28a853ab3899703f7b057c90c497152e72333
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2023-08-17 00:14:24 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,10 +1,5 @@
 function solution(sides) {
     sides.sort((a,b) => b-a);
     sum = sides[1] + sides[2];
-    if(sum > sides[0]){
-        return 1;
-    }else{
-        return 2;
-    }
-    
+    return sum > sides[0] ? 1: 2;
 }
